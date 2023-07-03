70c3acad89b9beb72adde5ef35e4b0608373cfaa
alyalsayed <alyalsayed@kali.aly.com>
2023-07-03 14:08:08 +0200

    breaking : upload bonus task 102

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -1,38 +1,29 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "unistd.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 /**
- * infinite_while - a function that runs forever and returns nothing
- * Return: 0 in the end
-*/
-int infinite_while(void)
-{
-	while (1)
-	{
-		sleep(1);
-	}
-	return (0);
-}
-
-/**
- * main - the entry to a program that creats 5 zombie process
- * Return: 0 on sucess
-*/
+ * main - zombies
+ *
+ * Description: make five zombies
+ * Return: 0 for success
+ */
 int main(void)
 {
-	int children_processes = 0;
-	pid_t pid;
+	int i;
+	pid_t pidme;
 
-	while (children_processes < 5)
+	i = 0;
+	while (i < 5)
 	{
-		pid = fork();
-		if (!pid)
-			break;
-		printf("Zombie process created, PID: %i\n", (int)pid);
-		children_processes++;
+		pidme = fork();
+		if (pidme)
+			printf("Zombie process created, PID: %i\n", pidme);
+		else
+			exit(0);
+		i++;
 	}
-	if (pid != 0)
-		infinite_while();
+	sleep(100);
 	return (0);
 }
