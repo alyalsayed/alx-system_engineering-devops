205fcc097f505acea6bdffa05f82a342b6052f5f
alyalsayed <alyalsayed@kali.aly.com>
2023-07-04 14:01:51 +0200

    breaking :  updating

--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,5 +1,2 @@
 #!/usr/bin/env ruby
-# Ruby script accepts one argument and passes it to a regular expression, matching method
-# The regular expression must be only matching: capital letters
-
-puts ARGV[0].scan(/(?<=from:|to:|flags:).+?(?=\])/).join(',')
+puts ARGV[0].scan(/[A-Z]*/).join
